8b0c3b54d9593f94fa7f7b4b7cf5d8127f7ec252
Upasana Sharma <sharmaupasana823@gmail.com>
2025-01-11 15:25:36 +0100

    Feat: Welcome Page

--- app/src/main/java/com/example/plantapp/WelcomeActivity.java
+++ app/src/main/java/com/example/plantapp/WelcomeActivity.java
@@ -8,17 +8,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 public class WelcomeActivity extends AppCompatActivity {
+    private EditText userNameInput;
+    private Button nextButton;
+    private ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
+        // If no data exists, show welcome screen
         setContentView(R.layout.activity_welcome);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Initialize views
+        userNameInput = findViewById(R.id.userNameInput);
+        nextButton = findViewById(R.id.nextButton);
+        logo = findViewById(R.id.logo);
+        logo.setImageResource(R.drawable.logo);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName = userNameInput.getText().toString().trim();
+                if (userName.isEmpty()) {
+                    userNameInput.setError("Please enter your name");
+                    return;
+                }
+
+                // Start or Navigate PlantDetailsActivity and pass the username
+//
+            }
         });
     }
+
 }
